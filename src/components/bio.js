8a9056a003d8ba910e2754a40f30c8009d6b7241
Devin Metivier <devinmetivier@gmail.com>
2019-03-12 10:28:09 -0400

    moving things around and changing configs, also edited bio (no need for class component)

--- src/components/bio.js
+++ src/components/bio.js
@@ -2,33 +2,29 @@ import React from 'react';
 import profilePic from '../assets/profile-pic.jpg';
 import { rhythm } from '../utils/typography';
 
-class Bio extends React.Component {
-  render() {
-    return (
-      <div
+export default function Bio() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        marginBottom: rhythm(2),
+      }}
+    >
+      <img
+        src={profilePic}
+        alt='Devin Metivier'
         style={{
-          display: 'flex',
-          marginBottom: rhythm(2),
+          marginRight: rhythm(1 / 2),
+          marginBottom: 0,
+          width: rhythm(2),
+          height: rhythm(2),
+          borderRadius: '50%',
         }}
-      >
-        <img
-          src={profilePic}
-          alt='Devin Metivier'
-          style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
-            borderRadius: '50%',
-          }}
-        />
-        <p style={{ maxWidth: 310 }}>
+      />
+      <p style={{ maxWidth: 310 }}>
           Personal blog by{' '}
-          <a href='https://mobile.twitter.com/devjmetivier'>Devin Metivier</a>.{' '}
-        </p>
-      </div>
-    );
-  }
+        <a href='https://mobile.twitter.com/devjmetivier'>Devin Metivier</a>.{' '}
+      </p>
+    </div>
+  );
 }
-
-export default Bio;
