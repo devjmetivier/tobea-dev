c47b0455faeab312e8e57117c35a1092a8f3388f
Devin Metivier <devinmetivier@gmail.com>
2019-03-26 13:36:45 -0400

    new stuff

---
+++ gatsby-config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  siteMetadata: {
+    title: `To Be A Dev`,
+    author: `Devin Metivier`,
+    description: `Personal Blog by Devin Metivier.`,
+    siteUrl: `https://tobea.dev`,
+    social: {
+      twitter: `@devjmetivier`,
+    },
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-mdx`,
+      options: {
+        // Apply gatsby-mdx to both .mdx and .md files
+        extensions: ['.mdx', '.md'],
+        defaultLayout: require.resolve('./src/components/blog-post-layout.js'),
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/pages/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+};
