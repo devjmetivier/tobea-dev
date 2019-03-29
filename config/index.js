f0177ee1ed5af703b3aacca8d868ed332738fb6d
Devin Metivier <devinmetivier@gmail.com>
2019-03-29 13:52:34 -0400

    oops

---
+++ config/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  pathPrefix: `/`, // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: `To Be A Dev`, // Navigation and Site Title
+  siteTitleAlt: `To Be A Dev - Development Blog`, // Alternative Site title for SEO
+  siteTitleManifest: `ToBeADev`,
+  siteUrl: `https://minimal-blog.lekoarts.de`, // Domain of your site. No trailing slash!
+  siteLanguage: `en`, // Language Tag on <html> element
+  siteHeadline: `Writing and publishing web development content`, // Headline for schema.org JSONLD
+  // siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  // favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
+  siteDescription: `Minimal Blog with a focus on JavaScript. Powered by MDX.`, // Your site description
+  author: `Devin metivier`, // Author for schemaORGJSONLD
+  // siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: `@devjmetivier`, // Twitter Username - Optional
+  ogSiteName: `tobeadev`, // Facebook Site Name - Optional
+  ogLanguage: `en_US`, // Facebook Language
+  // googleAnalyticsID: 'UA-47519312-6',
+
+  // Manifest and Progress color
+  // See: https://developers.google.com/web/fundamentals/web-app-manifest/
+  themeColor: `#3498DB`,
+  backgroundColor: `#2b2e3c`,
+};
---
+++ config/theme.js
@@ -0,0 +1,41 @@
+const colors = {
+  primary: `#d02e77`, // Color for buttons or links
+  bg: `white`, // Background color
+  grey: {
+    dark: `rgba(0, 0, 0, 0.9)`,
+    default: `rgba(0, 0, 0, 0.7)`,
+    light: `rgba(0, 0, 0, 0.5)`,
+    ultraLight: `rgba(0, 0, 0, 0.25)`,
+  },
+  white: `white`,
+};
+
+const transitions = {
+  normal: `0.5s`,
+};
+
+const fontSize = {
+  small: `0.9rem`,
+};
+
+const fontFamily = {
+  serif: `'Bitter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', serif`,
+  sansSerif: `'Open Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', sans-serif`,
+};
+
+const breakpoints = {
+  tablet: `1200px`,
+  phone: `600px`,
+};
+
+const theme = {
+  colors,
+  transitions,
+  fontSize,
+  breakpoints,
+  fontFamily,
+  maxWidth: `1000px`,
+  baseFontSize: `18px`,
+};
+
+export default theme;
