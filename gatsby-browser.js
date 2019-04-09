de0d5fa68fef0d7e3b27c921fcc31f0203b70c28
Devin Metivier <devinmetivier@gmail.com>
2019-04-09 15:58:15 -0400

    updating page progress indicator

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,10 +6,11 @@ import theme from './config/theme';
 
 export const wrapRootElement = wrap;
 
-// TODO: Make PR to plugin author, and/or consider publishing own plugin
-// https://github.com/barrymcgee/gatsby-plugin-scroll-indicator
+// TODO: extract code below and adapt to new gatsby plugin (gatsby-plugin-page-progress)
 const defaultOptions = {
-  applyToPrefix: ['post'],
+  prefixMatch: ['post'],
+  height: 3,
+  prependToBody: false,
   color: theme.colors.carmine,
 };
 
@@ -17,39 +18,47 @@ export const onRouteUpdate = (
   { location: { pathname } },
   pluginOptions = {}
 ) => {
-  // Merge default options with user defined options in `gatsby-config.js`
+  // merge default options with user defined options in `gatsby-config.js`
   const options = { ...defaultOptions, ...pluginOptions };
 
+  // TODO: use conditional to check if there are any options to match - if not, run on every page
+  if (!options.prefixMatch || options.prefixMatch === []) {
+    console.log('empty');
+  }
+
   // grab array of prefixes to apply progress to and make a new string for the RegExp
-  const prefixesToMatch = options.applyToPrefix.reduce(
+  const prefixesToMatch = options.prefixMatch.reduce(
     (accumulator, currentValue, i) =>
       i === 0 ? currentValue : `${accumulator}|${currentValue}`
   );
 
+  // should match to something like: (post|category|blog|etc)
   const re = RegExp(`^/(${prefixesToMatch})`, `gm`);
   const matches = re.test(pathname);
 
-  // check to see if the scroll indicator already exists
-  const indicatorCheck = document.getElementById(
-    'gatsby-plugin-scroll-indicator'
-  );
+  // check to see if the scroll indicator already exists - if it does, remove it
+  const indicatorCheck = document.getElementById('gatsby-plugin-page-progress');
   if (indicatorCheck) indicatorCheck.remove();
 
   if (matches) {
-    // Create indicator container and append to document body
+    // create progress indicator container and append/prepend to document body
     const node = document.createElement(`div`);
-    node.id = `gatsby-plugin-scroll-indicator`;
-    document.body.append(node);
+    node.id = `gatsby-plugin-page-progress`;
+    // eslint-disable-next-line
+    options.prependToBody
+      ? document.body.prepend(node)
+      : document.body.append(node);
 
+    // set defaults and grab progress indicator from the DOM
     let scrolling = false;
-    const indicator = document.getElementById('gatsby-plugin-scroll-indicator');
+    const indicator = document.getElementById('gatsby-plugin-page-progress');
 
-    // Determine width of scroll indicator
+    // determine width of progress indicator
     const getIndicatorPercentageWidth = (currentPos, totalScroll) => {
       return Math.floor((currentPos / totalScroll) * 100);
     };
 
-    // Find the total height of scrolling window
+    // find the total height of window
     const getScrollHeight = () => {
       // https://javascript.info/size-and-scroll-window#width-height-of-the-document
       return Math.max(
@@ -62,24 +71,26 @@ export const onRouteUpdate = (
       );
     };
 
-    // Add throttled listener to update on scroll
-    window.addEventListener('scroll', function() {
+    // add throttled listener to update on scroll
+    window.addEventListener('scroll', () => {
       const currentPos = window.scrollY;
       const { innerHeight } = window;
       const scrollHeight = getScrollHeight();
       const scrollDistance = scrollHeight - innerHeight;
+
       if (!scrolling) {
-        window.requestAnimationFrame(function() {
+        window.requestAnimationFrame(() => {
           const indicatorWidth = getIndicatorPercentageWidth(
             currentPos,
             scrollDistance
           );
+
           indicator.setAttribute(
             'style',
-            `width: ${indicatorWidth}%;position: fixed;height: 3px;background-color: ${
-              options.color
-            };top: 0;left: 0;transition:width 0.25s`
+            // eslint-disable-next-line
+            `width: ${indicatorWidth}%; position: fixed; height: ${options.height}px; background-color: ${options.color}; top: 0; left: 0; transition: width 0.25s`
           );
+
           scrolling = false;
         });
         scrolling = true;
