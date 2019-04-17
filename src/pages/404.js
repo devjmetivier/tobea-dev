1c8f17143f653791691dcf1c75d7750c122e726b
Devin Metivier <devinmetivier@gmail.com>
2019-04-17 15:43:14 -0400

    italicize keywords, 404 page created

---
+++ src/pages/404.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Layout, Bio } from '../components';
+
+const SoiledIt = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  span {
+    font-size: 5rem;
+  }
+`;
+
+const NotFound = ({ location }) => {
+  return (
+    <Layout location={location}>
+      <SoiledIt>
+        <h2>Page Doesn't Exist</h2>
+        <span>😢</span>
+        <h2>Doin' A Sad...</h2>
+      </SoiledIt>
+      <Bio />
+    </Layout>
+  );
+};
+
+export default NotFound;
