7aa05005a2cc702b67ef859ca44878998eefb05a
Devin Metivier <devinmetivier@gmail.com>
2019-05-22 10:25:19 -0400

    Don't like how this is turning out. Starting over with suggesting to use hooks
    Signed-off-by: Devin Metivier <devinmetivier@gmail.com>

--- blog/stringing-context-providers-together-in-react/src/context/GoodBoiContext.js
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useState } from 'react';
-
-export const PatsContext = createContext();
-
-export function PatsProvider(props) {
-  const [pats, givePats] = useState(0);
-  const value = React.useMemo(() => [pats, givePats], [pats]);
-  return <PatsContext.Provider value={value} {...props} />;
-}
